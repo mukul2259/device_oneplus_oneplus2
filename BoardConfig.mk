e60c064ae4aa258e50649faa888ea8802edafec9
Kilian von Pflugk <github@jumoog.io>
2018-12-10 12:31:27 +0100

    oneplus2: Remove TARGET_USES_64_BIT_BINDER
    The TARGET_USES_64_BIT_BINDER option is deprecated and can be removed
    
    Change-Id: Ieab43c6baf17eff134a11312d8f8ee7d1b310d55

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,8 +53,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-TARGET_USES_64_BIT_BINDER := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
