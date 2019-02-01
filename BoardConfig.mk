73ca5212a5d703770968d0ecb65db22ce6c765c7
Tri Vo <trong@google.com>
2019-02-01 11:27:25 -0800

    oneplus2: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
    We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
    extends system sepolicy. To that end, we use another make variable to
    specify product sepolicy.
    
    Bug: 119305624
    Test: m selinux_policy
    Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/private
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
