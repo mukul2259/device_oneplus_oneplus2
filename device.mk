e4589ad8cf72b39d6e8dcea9d106ee6c867c6102
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-10-14 14:15:42 +0530

    oneplus2: libshims: Fix IMS shim build for Q

--- device.mk
+++ device.mk
@@ -196,6 +196,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     ims-ext-common_system
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext \
