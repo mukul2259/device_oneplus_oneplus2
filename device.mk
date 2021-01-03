010b5d8777a6312f4b9887765d9f0902c040e2fe
Bruno Martins <bgcngm@gmail.com>
2021-01-03 11:17:21 +0000

    oneplus2: Deduplicate handheld_core_hardware.xml copy rule
    Since Android 10 that this is already done while inheriting from
    full_base_telephony.mk.
    
    Change-Id: If533fbf95a9cf9798ddba24955a624d26f550d62

--- device.mk
+++ device.mk
@@ -54,8 +54,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.software.sip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
