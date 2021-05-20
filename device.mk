5ffdfa7592683065fe929021c589f84eaa5cc19c
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2021-05-20 22:10:26 +0300

    oneplus2: Build mock hidl power stats
    * Fix for hidl power stats errors.
    - Now with this we fixing both errors:
    BatteryStatsService: Unable to load Power Hal or power.stats HAL
    hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.
    
    Change-Id: If691078015f00db31b6613bafbd5ca4dc3246d5c

--- device.mk
+++ device.mk
@@ -263,7 +263,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Privapp Whitelist
 PRODUCT_COPY_FILES += \
