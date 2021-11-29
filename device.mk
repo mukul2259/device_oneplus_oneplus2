8e6e8bfa5264763a97cdf0d861f2b7c8d249b8f8
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-11-29 07:31:18 +0000

    oneplus2: Remove duplicate SIP+VoIP permission
    This is copied in vendor/lineage/config/common.mk
    
    Change-Id: Ida45932cdf0505e7295a4af5128d73ac03b34d3e

--- device.mk
+++ device.mk
@@ -53,8 +53,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
-    frameworks/native/data/etc/android.software.sip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+    frameworks/native/data/etc/android.software.sip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
