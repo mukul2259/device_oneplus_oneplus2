cf70c7d9d2a027abdaaf766bb333ba1fc1d1130d
ivanmeler <i_ivan@windowslive.com>
2021-12-08 11:33:07 +0000

    oneplus2: move to ueventd to $(TARGET_OUT_VENDOR_ETC)
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Change-Id: I95e7aa2428c5c396bdd01e55b8fb7f8fb9bb5699

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -47,7 +47,7 @@ LOCAL_MODULE		:= ueventd.qcom.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 # Configuration scripts
