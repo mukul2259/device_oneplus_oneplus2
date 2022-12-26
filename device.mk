654a431e4b07c289985cc4e84b8dc838054f6d36
mukul2259 <dhirmukul3@gmail.com>
2022-12-26 22:49:59 +0000

    oneplus2: Cleanup offending entries
    build/make/core/main.mk:1308: warning:  device/oneplus/oneplus2/lineage_oneplus2.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    android.hardware.graphics.composer@2.1-impl
    libcnefeatureconfig
    libhidltransport.vendor
    libhwbinder.vendor
    librecovery_updater_oppo
    build/make/core/main.mk:1308: error: Build failed.

--- device.mk
+++ device.mk
@@ -137,7 +137,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
@@ -190,9 +189,7 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
     libhidltransport \
-    libhidltransport.vendor \
-    libhwbinder \
-    libhwbinder.vendor
+    libhwbinder
 
 # IMS
 PRODUCT_PACKAGES += \
@@ -306,7 +303,6 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     libaudioclient_shim \
-    libcnefeatureconfig \
     librmnetctl \
     libxml2 \
     rild_socket
