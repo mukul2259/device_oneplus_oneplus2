dc3a668d5b8085a7f32020c27ccf8e60a0b869ad
mukul2259 <dhirmukul3@gmail.com>
2023-04-10 01:48:56 +0200

    Revert "oneplus2: loggy: Only work with normal logs"
    This reverts commit ebdd0ba5980dbc3991ef8806a536dde07c1cb2e3.

--- rootdir/etc/loggy.sh
+++ rootdir/etc/loggy.sh
@@ -3,7 +3,7 @@
 
 date=`date +%F_%H-%M-%S`
 logcat -b all -v time -f  /cache/logcat_${date}.txt &
-# logcat -b radio -v time -f  /cache/radio_${date}.txt &
-# cat /dev/kmsg > /cache/kmsg_${date}.txt &
-# cp /data/tombstones > /cache/tombstones_${date} &
-# cat /proc/last_kmsg > /cache/last_kmsg${date}.txt
+logcat -b radio -v time -f  /cache/radio_${date}.txt &
+cat /dev/kmsg > /cache/kmsg_${date}.txt &
+cp /data/tombstones > /cache/tombstones_${date} &
+cat /proc/last_kmsg > /cache/last_kmsg${date}.txt
