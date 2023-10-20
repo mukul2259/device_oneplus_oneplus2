3f5375343772a539c8bd3c3457cfe5a3d6f20976
Mukul Dhir <dhirmukul3@gmail.com>
2023-10-20 11:52:47 +0000

    [SQUASHREVERT] oneplus2: Drop DRM clearkey plugins & updates
     [1] - Causing OTT apps like Hotstar to be stuck at splash screen, will revisit later
     [2] - Fixed apps on clearkey AIDL, but following appears in logs, so let's just revert back to what works fine
           10-20 19:37:12.719 E/clearkey-DrmFactory(471): Clearkey Drm HAL: failed to create drm plugin, invalid crypto scheme
    
    Revert "oneplus2: Drop default DRM service"
    
    This reverts commit 9e58b2bd262b4ccc53bbd9f5277b0af678640e23.
    
    Revert "msm8974-common: Use Aidl drm clearkey"
    
    This reverts commit a0969b13a510c2d5d08975191997445e77d5f118.
    
    Revert "oneplus2: Uprev DRM HIDL to 1.4"
    
    This reverts commit a0379b3975e5be4eec27655fa5c5ef3975244448.
    
    Revert "oneplus2: Enable v1.3 clearkey drm plugin"
    
    This reverts commit 022ba2a5ac24ec92fca2aa21d2ab68fe18068ab4.
    
    Revert "oneplus2: Build Clearkey 1.2 plugin and update vintf"
    
    This reverts commit b0d15bc138671f3503f85fc026822a206e8a9a49.
    
    Change-Id: I30b3773826edc72e4fa1cdf87f31a82442b62c51

--- device.mk
+++ device.mk
@@ -133,7 +133,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
