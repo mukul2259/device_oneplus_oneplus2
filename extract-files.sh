15aa22475ac44f76bfbbc233c4cb8f41aff2bfa4
Mukul Dhir <dhirmukul3@gmail.com>
2023-10-20 13:36:54 +0000

    [SQUASHREVERT] oneplus2: Revert back to patchelf state
    01-01 14:13:27.743   472   472 E android.hardware.drm@1.0-impl: Failed to lookup symbol createCryptoFactory in library /vendor/lib/mediadrm/libwvdrmengine.so: dlopen failed: cannot locate symbol "_ZN6google8protobuf8internal13empty_string_E" referenced by "/system/vendor/lib/mediadrm/libwvdrmengine.so"...
    
    Revert "oneplus2: Add protobuf vendorcompat lib"
    
    This reverts commit 796952b75c64b8bf60b1606caa4fa0386b8a8f49.
    
    Revert "oneplus2: Remove protobuf hax"
    
    This reverts commit e40f2ac1c568299fdf5ec532db328641d2b49370.

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,17 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+    vendor/lib/mediadrm/libwvdrmengine.so)
+        patchelf --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v28.so" "${2}"
+    ;;
+    vendor/lib64/libsettings.so)
+        patchelf --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v28.so" "${2}"
+    ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
