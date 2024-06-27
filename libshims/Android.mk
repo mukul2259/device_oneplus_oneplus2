07fa57ad7c3738d165d38dcf30507f288e7cfd1a
Mukul Dhir <dhirmukul3@gmail.com>
2024-06-27 21:51:25 +0200

    oneplus2: libshims: Fix compilation
    device/oneplus/oneplus2/libshims/Android.mk: error: "libaudioclient_shim (native:vendor) can not link against libaudioclient (native:platform)"
    device/oneplus/oneplus2/libshims/Android.mk: error: "libaudioclient_shim (native:vendor) can not link against libaudioclient (native:platform)"
    device/oneplus/oneplus2/libshims/Android.mk: error: "libshims_ims (native:vendor) can not link against libgui.vendor (native:vndk_private)"
    device/oneplus/oneplus2/libshims/Android.mk: error: "libshims_ims (native:vendor) can not link against libmedia (native:platform)"

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -38,7 +38,6 @@ LOCAL_SRC_FILES := libaudioclient_shim.cpp
 LOCAL_MODULE := libaudioclient_shim
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_SHARED_LIBRARIES := libaudioclient
-LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -51,7 +50,6 @@ LOCAL_SHARED_LIBRARIES := libstagefright_foundation libui libgui libmedia
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 LOCAL_MULTILIB := 64
-LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
 
