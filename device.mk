69c3fce02c7304ef2c660f6e73cf92fbbde2672e
Bruno Martins <bgcngm@gmail.com>
2024-08-24 09:56:20 +0100

    oneplus2: Build legacy vibrator AIDL HAL
    Change-Id: I2d6f2078e6edae203559ba57f82042b48917b418

--- device.mk
+++ device.mk
@@ -363,8 +363,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator-service.legacy
 
 # VNDK
 PRODUCT_PACKAGES += \
