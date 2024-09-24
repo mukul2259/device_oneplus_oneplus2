dfc21616fc64e739452a079acfde9c7c4b4d5c89
mukul2259 <dhirmukul3@gmail.com>
2024-09-24 20:28:32 +0200

    Revert "oneplus2: Patch keystore and libcrypto to use libcrypto-v33.so"
     * Not needed, boots and fingerprint works fine without it
    
    This reverts commit 50215574c936d8b5534fc8bc2f881ac654e1208d.

--- device.mk
+++ device.mk
@@ -356,9 +356,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VNDK
-PRODUCT_PACKAGES += \
-    libcrypto-v33
-
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
 
