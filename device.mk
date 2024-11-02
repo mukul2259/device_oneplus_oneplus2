304ac6b08b1637c8943c63a81f0ffbe99007fde4
LuK1337 <priv.luk@gmail.com>
2024-11-02 12:45:07 +0100

    oneplus2: Build libhidlbase-v32{,.vendor}
    Change-Id: I1b31199ec389ea6ba0d8faea4c539839c1cb8170

--- device.mk
+++ device.mk
@@ -360,10 +360,9 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-    libutils-v33
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+    libutils-v33 \
+    libhidlbase-v32 \
+    libhidlbase-v32.vendor
 
 # WiFi
 PRODUCT_PACKAGES += \
